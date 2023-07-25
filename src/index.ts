5616d1cfdc4bf5384ebe0e68d7febb7669a4db9d
LucieneFagundes <lfnascimento99@gmail.com>
2023-07-24 23:48:13 -0300

    :sparkles: feat:  Fibonacci com Emoji

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,58 @@
-// Exemplo: imprimir os primeiros 10 números da sequência de Fibonacci
-var n = 12;
-var result = fibonacci(n);
-console.log(`Entrada: ${n} `);
-var resultWithEmoji = replaceToEmoji(result);
-console.log(`Saída: ${resultWithEmoji}`);
+import promptSync from "prompt-sync";
+const prompt = promptSync();
+
+function startApp() {
+  console.log("Bem-vindo ao Fibonacci Emoji!\n");
+
+  do {
+    var response = prompt("Digite um número maior que 0 (ou 'sair' para encerrar): ");
+
+    if (response.toLowerCase() === 'sair') {
+      console.log("Encerrando o programa. Até logo!");
+      break;
+    }
+
+    const numberInput = Number(response);
+
+    if (isNaN(numberInput) || numberInput <= 0) {
+      console.log("Por favor, digite um número válido maior que 0.");
+      continue;
+    }
+
+    var result = fibonacci(numberInput);
+
+    console.log(`Entrada: ${numberInput}`);
+    var resultWithEmoji = replaceToEmoji(result);
+    console.log(`Saída: ${resultWithEmoji} \n`);
+  } while (true);
+}
 
 function fibonacci(n: number): number[] {
-	if (n === 0) {
-		return [];
-	} else if (n === 1) {
-		return [0];
-	} else if (n === 2) {
-		return [0, 1];
-	} else {
-		var sequence = [0, 1];
-		for (var i = 2; i < n + 1; i++) {
-			sequence.push(sequence[i - 1] + sequence[i - 2]);
-		}
-		return sequence;
-	}
+  if (n === 0) {
+    return [];
+  } else if (n === 1) {
+    return [0];
+  } else if (n === 2) {
+    return [0, 1];
+  } else {
+    var sequence = [0, 1];
+    for (var i = 2; i < n + 1; i++) {
+      sequence.push(sequence[i - 1] + sequence[i - 2]);
+    }
+    return sequence;
+  }
 }
 
 function replaceToEmoji(content: number[]) {
-	const emojis = ["🚀", "☘️", "🧑🏼‍🚀", "🤘🏼", "🇧🇷"];
+  const emojis = ["🚀", "☘️", "🧑🏼‍🚀", "🤘🏼", "🇧🇷", ];
 
-	const arrayWithEmojis = content.map((number) =>
-		number % 2 === 0 ? emojis[number % emojis.length] : number
-	);
+  const arrayWithEmojis = content.map((number) =>
+    number % 2 === 0 ? emojis[number % emojis.length] : number
+  );
 
-  const result = arrayWithEmojis.join(" - ")
+  const result = arrayWithEmojis.join(" - ");
 
-	return result;
+  return result;
 }
+
+startApp();
